3ba24729246855501a4df10d7177dd4efade4259
Rain <finn3004@gmail.com>
2026-01-12 16:44:56 +0100

    basic movement
    Added basic horizontal movement. speed can jump height might need to be adjusted

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,9 +4,12 @@ public class Movement : MonoBehaviour
 {
     public Rigidbody body;
     [SerializeField] float JumpForce;
+    [SerializeField] float MovementSpeed;
+    [SerializeField] float MaxSpeed;
     private bool JumpRequested = false;
     private int JumpTimes = 2;
     private bool isGrounded;
+
     void Start()
     {
         
@@ -21,13 +24,24 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         isGrounded = Physics.Raycast(body.position, Vector3.down, 1.1f);
-        Debug.DrawRay(body.position, Vector3.down * 1.1f, Color.red, 0, false);
+        Debug.DrawRay(body.position, Vector3.down * 0.6f, Color.red, 0, false);
 
         if (JumpRequested == true && isGrounded == true)
         {
-            body.AddForce(0, JumpForce, 0);
+            body.AddForce(0, JumpForce, 0, ForceMode.VelocityChange);
             JumpTimes--;
             JumpRequested = false;
+        }
+
+        if (Input.GetKey("d") && body.linearVelocity.magnitude < MaxSpeed)
+        {
+            body.AddForce(Vector3.right * MovementSpeed);
+            Debug.Log(body.linearVelocity.magnitude);
+        }
+        if (Input.GetKey("a") && body.linearVelocity.magnitude < MaxSpeed)
+        {
+            body.AddForce(Vector3.left * MovementSpeed);
+            Debug.Log(body.linearVelocity.magnitude);
         }
 
     }

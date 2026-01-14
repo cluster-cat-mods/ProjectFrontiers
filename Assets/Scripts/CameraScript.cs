d1c9b7a4775a26861adf62fdea6e684a0b7f9b02
cluster-cat-mods <casjava@outlook.com>
2026-01-14 13:57:55 +0100

    Improved camerasystem

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,19 +6,17 @@ using UnityEngine.Rendering;
 
 public class CameraScript : MonoBehaviour
 {
-    [SerializeField] private float distance = 5f;
     [SerializeField] private GameObject startPoint;
 
     private bool zoomBool = false;
     private Camera cam;
     private CinemachineCamera vcam;
     private Transform followTransform;
-    private Plane cursorPlane;
 
     public void DoCameraZoom(GameObject objectP)
     {
         zoomBool = !zoomBool;
-        vcam.ForceCameraPosition(objectP.transform.GetChild(0).position, objectP.transform.GetChild(0).rotation);
+        vcam = objectP.transform.GetChild(0).GetComponent<CinemachineCamera>();
         if (objectP != startPoint)
         {
             vcam.LookAt = objectP.transform;
@@ -39,6 +37,7 @@ public class CameraScript : MonoBehaviour
         }
 
         GameObject target = new GameObject("CinemachineCursorTarget");
+        target.transform.position = cam.transform.position + new Vector3(0, 0, 5);
         followTransform = target.transform;
         vcam.LookAt = followTransform;
     }
@@ -46,27 +45,20 @@ public class CameraScript : MonoBehaviour
     private void Update()
     {
         //looking
-        cursorPlane = new Plane(cam.transform.forward, cam.transform.position + cam.transform.forward * distance);
-        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        Vector3 mousPos = Input.mousePosition;
+        Ray ray = cam.ScreenPointToRay(mousPos);
+        Physics.Raycast(ray, out RaycastHit hit);
 
-        if (!zoomBool && cursorPlane.Raycast(ray, out float enter))
+        
+        if (!zoomBool)
         {
-            followTransform.position = ray.GetPoint(enter);
-        }
-
-
-        //actions
-        if (Input.GetMouseButtonDown(0))
-        {
-            if (Physics.Raycast(ray, out RaycastHit hit))
+            followTransform.position = hit.point;
+            if (Input.GetMouseButtonDown(0) && hit.collider != null && hit.collider.tag == "zoomObject")
             {
-                Debug.Log("Clicked: " + hit.collider.name);
-                if (!zoomBool && hit.collider.tag == "zoomObject")
-                {
-                    DoCameraZoom(hit.collider.GameObject());
-                }
+                DoCameraZoom(hit.collider.GameObject());
             }
         }
+
         if (Input.GetKey(KeyCode.Escape) && zoomBool)
         {
             DoCameraZoom(startPoint);
@@ -75,8 +67,21 @@ public class CameraScript : MonoBehaviour
 
     private void OnDrawGizmos()
     {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Physics.Raycast(ray, out RaycastHit hit);
+
         Gizmos.color = Color.yellow;
         Gizmos.DrawSphere(followTransform.position, 0.5f);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawLine(ray.origin, hit.point);
+        Gizmos.color = Color.gray;
+        Gizmos.DrawSphere(ray.origin, 0.25f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(hit.point, 0.25f);
+
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(Input.mousePosition + new Vector3(Screen.width / 2, Screen.height / 2, 0), 0.25f);
     }
 
 }
